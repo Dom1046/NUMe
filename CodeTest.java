8d8049a239b7ff4bb4038351cc630ab71267b55e
Dom1046 <rkrkwnj1046@gmail.com>
2024-08-14 23:20:41 +0900

    8/14 upload

---
+++ CodeTest.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class CodeTest {
+    public static void main(String[] args) {
+        String ranNum = "!@#$%^&*(\\\'\"<>?:";
+        System.out.println(ranNum);
+    }
+}
