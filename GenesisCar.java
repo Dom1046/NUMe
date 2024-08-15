afec3198229f448e84aabf2d06cbd33a665b01c2
Dom1046 <rkrkwnj1046@gmail.com>
2024-08-15 22:07:57 +0900

    8/15 upload4

--- GenesisCar.java
+++ GenesisCar.java
@@ -1,14 +1,7 @@
-public class GenesisCar{
-    private static void say(){
-        System.out.println("private static");
-    }
-    private void say1(){
-        System.out.println("private");
-        say();
-        say2();
-    }
-    static void say2(){
-        System.out.println("---static---");
-        say();
+public class GenesisCar {
+
+
+    public static void main(String[] args) {
+
     }
 }
--- Review.java
+++ Review.java
@@ -1,7 +1,37 @@
-public class Review{
-    public static void main(String[] args) {
-        GenesisCar genesisCar = new GenesisCar();
-        System.out.println("System.identityHashCode(genesisCar) = " + Integer.toHexString(System.identityHashCode(genesisCar)));
+import java.util.*;
+
+public class Review {
+    int times= 1;
+    int count = 0;
+    int sum = 0;
+    public int solution(String[][] clothes) {
+//        String[][] clothes = new String[n][m];
+        for (int i = 0; i < clothes.length; i++) {
+            sum += clothes[i].length;
+            for (int j = 0; j < clothes.length; j++) {
+                if ((i != j) && (j > i)) {
+                    times *= clothes[j].length;
+                }
+            }
+        }
     }
 
+    public int volution(String[][] clothes) {
+        if (clothes.length == 0) {
+            return count = 1;
+        }
+        int x = 0;
+        for (int j = 0; j < clothes.length; j++) {
+            if ((x != j) && (j > x)) {
+                sum += clothes[x].length * clothes[j].length;
+                x++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Review review = new Review();
+
+    }
 }
