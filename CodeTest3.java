22637b0e5cbb4a5e0c87edf8d49744421c9b7a39
Dom1046 <rkrkwnj1046@gmail.com>
2024-08-15 10:14:54 +0900

    8/15 upload2

--- CodeTest3.java
+++ CodeTest3.java
@@ -1,25 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class CodeTest3 {
     static BufferedReader br;
 
-    public String solution(String str1, String str2) {
-        String answer = "";
-        char[] array1 = str1.toCharArray();
-        char[] array2 = str2.toCharArray();
-        for(int i = 0; i <array1.length; i++){
-            answer = answer + array1[i]+array2[i];
-        }
-        return answer;
+    public int solution(int a, int b) {
+        int answer = 0;
+        String array1 = a+""+b;
+        String array2 = b+""+a;
+        return whoBig(array1, array2);
+    }
+    public int whoBig(String array1,String array2){
+        if (array1.equals(array2)){
+            return Integer.parseInt(array1);
+        }else if (Integer.parseInt(array1)>Integer.parseInt(array2)){
+            return Integer.parseInt(array1);
+        }else if (Integer.parseInt(array1)<Integer.parseInt(array2)){
+            return Integer.parseInt(array2);
+        }return -1;
     }
 
     public static void main(String[] args) throws IOException {
         CodeTest3 codeTest3 = new CodeTest3();
-        String array1 = "aaaa";
-        String array2 = "bbbb";
-        System.out.println("codeTest3.solution(array1, array2) = " + codeTest3.solution(array1, array2));
+        int a = 12;
+        int b = 2;
+        System.out.println("codeTest3.solution(a,b) = " + codeTest3.solution(a, b));
     }
 }
 //"He11oWor1d"	"lloWorl"	2	"HelloWorld"
